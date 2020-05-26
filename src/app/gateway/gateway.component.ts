bf8ff57c595193fbb14469abc74b224908567169
sivaiah1525 <sivaganga821@gmail.com>
2020-05-26 14:32:58 +0530

    add table && nav tatile alignment

--- src/app/gateway/gateway.component.ts
+++ src/app/gateway/gateway.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gateway.component.css']
 })
 export class GatewayComponent implements OnInit {
-
+  table: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.table = [
+      {
+        GatewayID: '123',
+        GatewayIP: '1234567',
+        NetWorkName: 'local',
+        Mechinename: 'network',
+        mechinepurpose: 'maintain',
+        MechineIP: '12345678',
+        CompanyName: 'Wipro'
+      },
+      {
+        GatewayID: '123',
+        GatewayIP: '1234567',
+        NetWorkName: 'local',
+        Mechinename: 'network',
+        mechinepurpose: 'maintain',
+        MechineIP: '12345678',
+        CompanyName: 'Wipro'
+      },
+      {
+        GatewayID: '123',
+        GatewayIP: '1234567',
+        NetWorkName: 'local',
+        Mechinename: 'network',
+        mechinepurpose: 'maintain',
+        MechineIP: '12345678',
+        CompanyName: 'Wipro'
+      },
+      {
+        GatewayID: '123',
+        GatewayIP: '1234567',
+        NetWorkName: 'local',
+        Mechinename: 'network',
+        mechinepurpose: 'maintain',
+        MechineIP: '12345678',
+        CompanyName: 'Wipro'
+      },
+      {
+        GatewayID: '123',
+        GatewayIP: '1234567',
+        NetWorkName: 'local',
+        Mechinename: 'network',
+        mechinepurpose: 'maintain',
+        MechineIP: '12345678',
+        CompanyName: 'Wipro'
+      },
+    ];
+
   }
 
 }
