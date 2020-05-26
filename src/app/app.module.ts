f7492ccf851dc70ba722221418ba4f363c56e571
sivaiah jg <sivagopi821@gmail.com>
2020-05-26 13:28:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { GatewayComponent } from './gateway/gateway.component';
+import { NetworkComponent } from './network/network.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NewNetworkComponent } from './new-network/new-network.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    GatewayComponent,
+    NetworkComponent,
+    ProfileComponent,
+    NewNetworkComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
